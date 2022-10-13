6ee9f2027cd9fc5a1cf30746966450197b069228
Kevin Lopez <kevinlopez@Kevins-MacBook-Air-2.local>
2022-10-13 10:11:08 -0700

    user authentication with passport and JSON web token

--- routes/partnersRouter.js
+++ routes/partnersRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Partner = require("../models/partner");
+const authenticate = require("../authenticate");
 const partnersRouter = express.Router();
 
 partnersRouter.route("/")
@@ -12,7 +13,7 @@ partnersRouter.route("/")
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Partner.create(req.body)
     .then(partner => {
         console.log("Partner Created ", partner);
@@ -22,11 +23,11 @@ partnersRouter.route("/")
     })
     .catch(err => next(err));
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end("PUT not supported");
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Partner.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -46,11 +47,11 @@ partnersRouter.route("/:partnerId")
     })
     .catch(err => next(err));
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`POST not supported on /partners/${req.params.partnerId}`);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Partner.findByIdAndUpdate(req.params.partnerId, {
         $set: req.body
     }, { new: true })
